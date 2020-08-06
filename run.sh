6c04435fa0d627a7bca1242aed2524ba8cea919f
Hao <hao@sporule.com>
2020-08-06 19:00:03 +0000

    change transfers and chekcers to parameters

--- run.sh
+++ run.sh
@@ -2,4 +2,6 @@
 
 PASSWORD = `rclone obscure $PASSWORD`
 
-rclone sync --progress $SOURCE :ftp:$TARGET --ftp-host=$SERVER --ftp-user=$USERNAME --ftp-pass=$PASSWORD --backup-dir :ftp:$BACKUPDIR --checkers 1 --transfers 1
+
+
+rclone sync --progress $SOURCE :ftp:$TARGET --ftp-host=$SERVER --ftp-user=$USERNAME --ftp-pass=$PASSWORD --backup-dir :ftp:$BACKUPDIR --checkers $CHECKERS --transfers $TRANSFERS
