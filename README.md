8047a856618f6ce8000ba836713308fbe508316d
Hao <hao@sporule.com>
2020-08-06 18:55:50 +0000

    updated the image

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# rclone-nas-backup
+# RClone NAS Backup
+
+This is a container image to sync the selected folders to target ftp server.
+The prefer environment is
--- run.sh
+++ run.sh
@@ -1,19 +1,5 @@
 #!/bin/sh
 
-echo $SOURCE
-echo $TARGET
-echo $SERVER
-echo $USERNAME
-echo $PASSWORD
+PASSWORD = `rclone obscure $PASSWORD`
 
-
-# # Run Airflow if it is enabled
-# if [ "$AIRFLOW" = "1" ]; then airflow initdb; (airflow webserver -p 8082 &) ;(airflow scheduler &); fi
-
-# # Run Jupyter Lab if it is enabled
-# if [ "$JUPYTER" = "1" ]; then (jupyter lab --ip 0.0.0.0 --allow-root --NotebookApp.token=$JUPYTERTOKEN --port 8080 &) ; fi
-
-# # Run Jupyter Lab if it is enabled
-# if [ "$NIFI" = "1" ]; then (service nifi start) ; fi
-
-tail -f /dev/null
+rclone sync --progress $SOURCE :ftp:$TARGET --ftp-host=$SERVER --ftp-user=$USERNAME --ftp-pass=$PASSWORD --backup-dir :ftp:$BACKUPDIR --checkers 1 --transfers 1
